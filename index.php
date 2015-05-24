e79090e706006a78e42041304a14be9a7a90946e
Florian Weber <florian.weber.dd@icloud.com>
2015-05-24 21:10:01 +0200

    bas

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * downloads.octostag.com
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='http://octostag.blogspot.de/favicon.ico' rel='icon' type='image/x-icon'/>
+
+        <title>Downloads &middot; Octostag</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/app.css">
+
+        <!-- JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+        <!-- FONTS-->
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow|Ubuntu|Open+Sans|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+
+        <div id="wrap">
+
+            <!-- HEADER -->
+            <div class="o-header-container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+
+                            <div class="o-logo o-logo-header">
+                                <h1>Octostag
+                                    <small>Downloads</small>
+                                </h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- CONTENT -->
+            <div class="o-content-container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 o-content o-font-open-sans">
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- FOOTER -->
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 o-footer o-font-open-sans">
+
+                        Octostag |
+	                    <a href="http://octostag.blogspot.de">Blog</a> |
+                        <a href="http://octostag.com">Tumblr</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
